bdeb86bb07dd0eb44e8507fe8ed63f807d0d6553
Alisher <aajk2008@gmail.com>
2013-08-15 23:32:07 -0500

    grad2

--- platforms/android/assets/www/js/employeelist.js
+++ platforms/android/assets/www/js/employeelist.js
@@ -1,5 +1,7 @@
 var db;
 var dbCreated = false;
+var ind = "";
+var v = "";
 
 var scroll = new iScroll('wrapper', { vScrollbar: false, hScrollbar:false, hScroll: false });
 
@@ -8,7 +10,7 @@ document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
     db = window.openDatabase("EmployeeDirectoryDB", "1.0", "PhoneGap Demo", 200000);
     if (dbCreated)
-    	db.transaction(getEmployees, transaction_error);
+    	db.transaction(getIndexes, transaction_error);
     else
     	db.transaction(populateDB, transaction_error, populateDB_success);
 }
@@ -20,61 +22,71 @@ function transaction_error(tx, error) {
 
 function populateDB_success() {
 	dbCreated = true;
-    db.transaction(getEmployees, transaction_error);
+    db.transaction(getIndexes, transaction_error);
 }
 
-function getEmployees(tx) {
-	var sql = "select e.id, e.firstName, e.lastName, e.title, e.picture, count(r.id) reportCount " + 
-				"from employee e left join employee r on r.managerId = e.id " +
-				"group by e.id order by e.lastName, e.firstName";
-	tx.executeSql(sql, [], getEmployees_success);
+function getIndexes(tx) {
+
+	var sql = "select e.id, e.Word, e.Definition " +
+				"from words e " +
+				"where e.Word LIKE '%" + ind + "%' order by e.Word LIMIT 64 ";
+	tx.executeSql(sql, [], getIndexes_success);
 }
 
-function getEmployees_success(tx, results) {
+function getDefinition(tx) {
+    var sql = "select e.Definition " +
+				"from words e " +
+				"where e.id = " + v;
+    tx.executeSql(sql, [], getDefinitions_success);
+}
+
+function getIndexes_success(tx, results) {
 	$('#busy').hide();
-    var len = results.rows.length;
-    for (var i=0; i<len; i++) {
-    	var employee = results.rows.item(i);
-		$('#employeeList').append('<li><a href="employeedetails.html?id=' + employee.id + '">' +
-				'<img src="pics/' + employee.picture + '" class="list-icon"/>' +
-				'<p class="line1">' + employee.firstName + ' ' + employee.lastName + '</p>' +
-				'<p class="line2">' + employee.title + '</p>' +
-				'<span class="bubble">' + employee.reportCount + '</span></a></li>');
-    }
+ 
+
+
+	var len = results.rows.length;
+	var s = "";
+	for (var i = 0; i < len; i++) {
+	    var word = results.rows.item(i);
+	    s += '<li><a id="' + word.id + '" class="click-word">' + word.Word + '</a></li>';
+	}
+	$('#employeeList').html(s);
+
+	$('.click-word').click(function () {
+	    v = $(this).attr('id');
+	    db.transaction(getDefinition, transaction_error);
+	});
+
 	setTimeout(function(){
 		scroll.refresh();
 	},100);
-	db = null;
+}
+
+function getDefinitions_success(tx, results) {
+    $('#busy').hide();
+    var word = results.rows.item(0);
+    $('#definitionList').html('<li>' + word.Definition + '</li>');
 }
 
 function populateDB(tx) {
 	$('#busy').show();
-	tx.executeSql('DROP TABLE IF EXISTS employee');
-	var sql = 
-		"CREATE TABLE IF NOT EXISTS employee ( "+
-		"id INTEGER PRIMARY KEY AUTOINCREMENT, " +
-		"firstName VARCHAR(50), " +
-		"lastName VARCHAR(50), " +
-		"title VARCHAR(50), " +
-		"department VARCHAR(50), " + 
-		"managerId INTEGER, " +
-		"city VARCHAR(50), " +
-		"officePhone VARCHAR(30), " + 
-		"cellPhone VARCHAR(30), " +
-		"email VARCHAR(30), " +
-		"picture VARCHAR(200))";
-    tx.executeSql(sql);
 
-    tx.executeSql("INSERT INTO employee (id,firstName,lastName,managerId,title,department,officePhone,cellPhone,email,city,picture) VALUES (12,'Steven','Wells',4,'Software Architect','Engineering','617-000-0012','781-000-0012','swells@fakemail.com','Boston, MA','steven_wells.jpg')");
-    tx.executeSql("INSERT INTO employee (id,firstName,lastName,managerId,title,department,officePhone,cellPhone,email,city,picture) VALUES (11,'Amy','Jones',5,'Sales Representative','Sales','617-000-0011','781-000-0011','ajones@fakemail.com','Boston, MA','amy_jones.jpg')");
-    tx.executeSql("INSERT INTO employee (id,firstName,lastName,managerId,title,department,officePhone,cellPhone,email,city,picture) VALUES (10,'Kathleen','Byrne',5,'Sales Representative','Sales','617-000-0010','781-000-0010','kbyrne@fakemail.com','Boston, MA','kathleen_byrne.jpg')");
-    tx.executeSql("INSERT INTO employee (id,firstName,lastName,managerId,title,department,officePhone,cellPhone,email,city,picture) VALUES (9,'Gary','Donovan',2,'Marketing','Marketing','617-000-0009','781-000-0009','gdonovan@fakemail.com','Boston, MA','gary_donovan.jpg')");
-    tx.executeSql("INSERT INTO employee (id,firstName,lastName,managerId,title,department,officePhone,cellPhone,email,city,picture) VALUES (8,'Lisa','Wong',2,'Marketing Manager','Marketing','617-000-0008','781-000-0008','lwong@fakemail.com','Boston, MA','lisa_wong.jpg')");
-    tx.executeSql("INSERT INTO employee (id,firstName,lastName,managerId,title,department,officePhone,cellPhone,email,city,picture) VALUES (7,'Paula','Gates',4,'Software Architect','Engineering','617-000-0007','781-000-0007','pgates@fakemail.com','Boston, MA','paula_gates.jpg')");
-    tx.executeSql("INSERT INTO employee (id,firstName,lastName,managerId,title,department,officePhone,cellPhone,email,city,picture) VALUES (5,'Ray','Moore',1,'VP of Sales','Sales','617-000-0005','781-000-0005','rmoore@fakemail.com','Boston, MA','ray_moore.jpg')");
-    tx.executeSql("INSERT INTO employee (id,firstName,lastName,managerId,title,department,officePhone,cellPhone,email,city,picture) VALUES (6,'Paul','Jones',4,'QA Manager','Engineering','617-000-0006','781-000-0006','pjones@fakemail.com','Boston, MA','paul_jones.jpg')");
-    tx.executeSql("INSERT INTO employee (id,firstName,lastName,managerId,title,department,officePhone,cellPhone,email,city,picture) VALUES (3,'Eugene','Lee',1,'CFO','Accounting','617-000-0003','781-000-0003','elee@fakemail.com','Boston, MA','eugene_lee.jpg')");
-    tx.executeSql("INSERT INTO employee (id,firstName,lastName,managerId,title,department,officePhone,cellPhone,email,city,picture) VALUES (4,'John','Williams',1,'VP of Engineering','Engineering','617-000-0004','781-000-0004','jwilliams@fakemail.com','Boston, MA','john_williams.jpg')");
-    tx.executeSql("INSERT INTO employee (id,firstName,lastName,managerId,title,department,officePhone,cellPhone,email,city,picture) VALUES (2,'Julie','Taylor',1,'VP of Marketing','Marketing','617-000-0002','781-000-0002','jtaylor@fakemail.com','Boston, MA','julie_taylor.jpg')");
-    tx.executeSql("INSERT INTO employee (id,firstName,lastName,managerId,title,department,officePhone,cellPhone,email,city,picture) VALUES (1,'James','King',0,'President and CEO','Corporate','617-000-0001','781-000-0001','jking@fakemail.com','Boston, MA','james_king.jpg')");
+
+	tx.executeSql('DROP TABLE IF EXISTS words');
+	var sql =
+		"CREATE TABLE IF NOT EXISTS words ( " +
+		"id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+		"Word VARCHAR(50), " +
+		"[Definition] VARCHAR(512))";
+	tx.executeSql(sql);
+  
+	tx.executeSql("INSERT INTO words (Word,[Definition]) VALUES ('cat','asdsad')");
+	tx.executeSql("INSERT INTO words (Word,[Definition]) VALUES ('dog','asdsadssa')");
+	tx.executeSql("INSERT INTO words (Word,[Definition]) VALUES ('bird','asdasdsad')");
+	tx.executeSql("INSERT INTO words (Word,[Definition]) VALUES ('bear','Wasdasdasells')");
+	tx.executeSql("INSERT INTO words (Word,[Definition]) VALUES ('kaka','wererwerwer')");
+	tx.executeSql("INSERT INTO words (Word,[Definition]) VALUES ('makaa','werwerwfds')");
+	tx.executeSql("INSERT INTO words (Word,[Definition]) VALUES ('saka','sdfsdgre')");
+	tx.executeSql("INSERT INTO words (Word,[Definition]) VALUES ('slon','werwrwegdfg')");
 }
